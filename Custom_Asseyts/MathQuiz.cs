45e421b5a4e8666c71291d17a0400a45b38e4772
Angel David <40445747+AngelDavidStudios@users.noreply.github.com>
2023-07-14 10:57:33 -0500

    Interfaz MathQuizz

---
+++ Custom_Asseyts/MathQuiz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Game;
+using UnityEngine;
+
+
+namespace Model
+{
+    [System.Serializable]
+    public struct MathQuiz
+    {
+        public string[] quiz;
+        public string[] answers;
+        public int correctIndex;
+        public Vector2Int hardnessRange;
+    }
+
+}
