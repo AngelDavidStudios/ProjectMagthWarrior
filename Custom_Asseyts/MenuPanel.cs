d781af32a7dcdc65d2e8b6327fe68d9e9f712998
Angel David <40445747+AngelDavidStudios@users.noreply.github.com>
2023-07-16 20:41:14 -0500

    Update Scripts

---
+++ Custom_Asseyts/MenuPanel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using MyGame;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game
+{
+    public class MenuPanel : ShowHidable
+    {
+        [SerializeField] private Text _bestScoreTxt;
+        [SerializeField] private string _bestScorePrefix;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            _bestScoreTxt.text = $"{_bestScorePrefix}{MyGame.GameManager.BEST_SCORE}";
+            
+        }
+
+        private void Start()
+        {
+    
+        }
+        
+        public void OnClickOnePlayer()
+        {
+            Hide();
+            GameManager.LoadGameOnePlayer(new LoadGameData());
+        }
+
+        public void OnClickMultiPlayer()
+        {
+            Hide();
+            GameManager.LoadGameMultiplayer(new LoadGameData());
+        }
+
+        public void OnClickAlgebra()
+        {
+            Hide();
+            GameManager.LoadGameAlgebra(new LoadGameData());
+        }
+
+        public void OnClickAritmetica()
+        {
+            Hide();
+            GameManager.LoadGameAritmetica(new LoadGameData());
+        }
+
+        public void OnClickGeometria()
+        {
+            Hide();
+            GameManager.LoadGameGeometria(new LoadGameData());
+        }
+
+        public void OnClickTrigo()
+        {
+            Hide();
+            GameManager.LoadGameTrigonometria(new LoadGameData());
+        }
+
+        public void OnClickMenu()
+        {
+            Hide();
+            GameManager.LoadScene("Menu");
+        }
+
+    }
+}
---
+++ Custom_Asseyts/SharedUIManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class SharedUIManager : Singleton<SharedUIManager>
+{
+
+    [SerializeField] private LoadingPanel _loadingPanel;
+
+    public static LoadingPanel LoadingPanel => Instance?._loadingPanel;
+}
